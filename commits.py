c5718506b5666a707dc225c3f94923835720353c
Luke Bakken <bakkenlab@gmail.com>
2019-02-23 13:33:52 -0700

    Talking to github API. Able to get dates in rang

---
+++ commits.py
@@ -0,0 +1,29 @@
+import requests
+import dateutil.parser as dateParser
+
+
+def get_commits():
+    url = ""
+    headers = {'Authorization': 'token '}
+    request = requests.get(url, headers=headers)
+    return request
+
+
+def get_commits_in_range(date_from, date_to):
+    commit_json = get_commits()
+
+    commit_arr = []
+    for commit in commit_json.json():
+        date = commit['commit']['committer']['date']
+        parsed_date = dateParser.parse(date)
+        hour = parsed_date.strftime('%H')
+        if int(hour) < date_from or int(hour) > date_to:
+            commit_arr.append(date)
+
+    return commit_arr
+
+
+if __name__ == '__main__':
+    dates = get_commits_in_range(9, 17)
+    print(dates)
+
